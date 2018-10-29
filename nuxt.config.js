95059840baccbed9125bd903f2b4a418d850ff02
maliaoMJ <1302151931@qq.com>
2018-10-29 17:14:45 +0800

    添加首页轮播图

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,12 +32,15 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'swiper/dist/css/swiper.min.css'
+  ],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: ['@/plugins/element-ui'],
+  plugins: ['@/plugins/element-ui', { src: '@/plugins/swiper', ssr: false }],
 
   /*
   ** Nuxt.js modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,45 @@
 <template>
-  <section class="container">
-    simple Vue SSR Template
-    <h1><i class="fa fa-home"/></h1>
-    <img
-      src="~/assets/images/avatar.jpg"
-      alt="userAvatar"
-      class="avatar">
-  </section>
+  <div class="container">
+    <div
+      v-swiper:mySwiper="swiperOption"
+      class="swiperContainer">
+      <div class="swiper-wrapper">
+        <div
+          v-for="banner in banners"
+          :key="banner"
+          class="swiper-slide">
+          <img :src="banner">
+        </div>
+      </div>
+      <div class="swiper-pagination"/>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   data() {
-    return {}
+    return {
+      banners: [
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540804892885&di=c3f5203561af85079cac742eadb7fd60&imgtype=0&src=http%3A%2F%2Ff2.dn.anqu.com%2Fdown%2FMjRiMA%3D%3D%2Fallimg%2F1212%2F17-12120G01253.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540804892885&di=26d958331eacff74c7a6cb4ece5e0e1d&imgtype=0&src=http%3A%2F%2Ff.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Feac4b74543a982267a3d54978a82b9014b90eb86.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540804892885&di=b5f61cd626ac98ec11ed36d868c8b912&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F38dbb6fd5266d016f543767c932bd40735fa3508.jpg' ],
+      swiperOption: {
+        direction: 'vertical',
+        effect: 'cube',
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        keyboard: {
+          keyboard: true
+        },
+        mousewheel: {
+          mousewheel: true
+        }
+      }
+    }
   }
 }
 </script>
@@ -25,5 +51,21 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.swiperContainer {
+  width: 100%;
+  height: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
