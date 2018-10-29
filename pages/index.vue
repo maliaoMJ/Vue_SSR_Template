97a41db1c8d71cd226ab9526e646194becd2cb60
maliaoMJ <1302151931@qq.com>
2018-10-29 13:50:13 +0800

    测试资源路径加载

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <section class="container">
     simple Vue SSR Template
     <h1><i class="fa fa-home"/></h1>
+    <img
+      src="~/assets/images/avatar.jpg"
+      alt="userAvatar"
+      class="avatar">
   </section>
 </template>
 
